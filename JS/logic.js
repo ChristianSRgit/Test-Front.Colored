28ec06c05cf12a62eef598781d87ad7712ba91e0
Christian.s.r.Home <christian.s.ramundo@gmail.com>
2022-08-04 20:41:28 -0300

    comments working

--- JS/logic.js
+++ JS/logic.js
@@ -1,34 +1,45 @@
 //Swiper
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
-  
-  //Comments
-  let inputText = document.getElementById("inputText");
-  let span = document.getElementById("span");
-  let postBtnListener = document.getElementById("postBtnListener");
-  inputText.addEventListener("keydown",textModified);
-  postBtnListener.addEventListener("click",postComment);
-  
-  function textModified(){
-    let comment = document.getElementById("comment2");
-    
-    comment.innerText = inputText.value;
-    //conflicto para devolver la palabra "comment" bold, revisar despues innerHTML func
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   }
-  
-  function postComment(){
-    
-  }
+});
+
+//CommentsSection
+let inputText = document.getElementById("inputText");
+let userWhoPost = "Comment";
+let commentNumber = 2;
+let commentID = `comment${commentNumber}`; //iterar para que una vez posteado el coment anterior el id ++
+let postBtnListener = document.getElementById("postBtnListener");
+var comment = document.getElementById(commentID);
+
+inputText.addEventListener("keyup", postComment/* textModified */);
+postBtnListener.addEventListener("click", postComment);
+
+function textModified() {
+  /* comment.innerHTML = userWhoPost.replace(/(^|\s)(Comment)(\s|$)/ig, '$1<b>$2</b>$3') + "&nbsp;" + inputText.value;
+ */
+}
+
+function postComment() {
+  comment.innerHTML = userWhoPost.replace(/(^|\s)(Comment)(\s|$)/ig, '$1<b>$2</b>$3') + "&nbsp;" + inputText.value;
+  if(inputText.value !== ""){
+    inputText.value = "";
+    comment.classList.add("postedComment");
+    comment.classList.remove("noPostedComment");
+    commentNumber ++
+}
+}
+
+//fetch api buscar
+//separar libreria swiper
